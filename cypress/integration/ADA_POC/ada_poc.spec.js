f9f77bca28cff7f8460c45d64ae3ae0fe2b16a2f
tzero86 <eliasmedina86@gmai.com>
2021-05-28 21:20:16 -0300

    Better logging 4 both console and cypress runner.

--- cypress/integration/ADA_POC/ada_poc.spec.js
+++ cypress/integration/ADA_POC/ada_poc.spec.js
@@ -1,42 +1,6 @@
-const terminalLog = (violations) => {
-    cy.task(
-      'log',
-      `${violations.length} accessibility violation${
-        violations.length === 1 ? '' : 's'
-      } ${violations.length === 1 ? 'was' : 'were'} detected`
-    )
-    // pluck specific keys to keep the table readable
-    cy.log('log', violations)
-    const violationData = violations.map(
-      ({ id, impact, description, nodes, html }) => ({
-        nodes: nodes.length,
-        id,
-        impact,
-        description,
-        //html: nodes[0].html,
-        //summary: nodes[0].failureSummary
-      })
-    )
-   
-    cy.task('table', violationData)
-  }
-
-
 describe('Automated Accessibility Testing POC', ()=>{
 
     it('This page should be accessible.', ()=>{
-        cy.visit('/');
-        cy.injectAxe();
-        cy.checkA11y(
-            '',
-            {
-              runOnly: {
-                type: 'tag',
-                values: ['wcag2a', 'wcag2aa'],
-              },
-            },
-            terminalLog,
-          );
-        
+        cy.testAccessibility('');
     })
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,75 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+const indicators = {
+    minor: '🟡',
+    moderate: '🟠',
+    serious: '🔴',
+    critical: '⛔',
+}
+
+function logViolations(violations) {
+    terminalLog(violations)
+    violations.forEach(violation => {
+        const nodes = Cypress.$(violation.nodes.map(node => node.target).join(','))
+        let log = {
+            name: `[${indicators[violation.impact]} ${violation.impact}]`,
+            consoleProps: () => violation,
+            $el: nodes,
+            message: `[${violation.help}](${violation.helpUrl})`
+
+        }
+        Cypress.log(log)
+
+        violation.nodes.forEach(({ target }) => {
+            Cypress.log({
+                name: '- 🩸 FIXME',
+                consoleProps: ()=> violation,
+                $el: Cypress.$(target.join(',')),
+                message: target
+            })
+
+        })
+    });
+}
+
+const terminalLog = (violations) => {
+    cy.task(
+      'log',
+      `${violations.length} accessibility violation${
+        violations.length === 1 ? '' : 's'
+      } ${violations.length === 1 ? 'was' : 'were'} detected`
+    )
+    // pluck specific keys to keep the table readable
+    cy.log('log', violations)
+    const violationData = violations.map(
+      ({ id, impact, description, nodes}) => ({
+        name: `${indicators[impact]} ${impact}`,
+        nodes: nodes.length,
+        id,
+        description,
+        //html: nodes[0].html,
+        //summary: nodes[0].failureSummary
+      })
+    )
+
+    cy.task('table', violationData)
+  }
+
+
+Cypress.Commands.add('testAccessibility', (path) => {
+    cy.visit(path)
+    cy.injectAxe()
+    cy.checkA11y(
+        null,
+        {
+          runOnly: {
+            type: 'tag',
+            values: ['wcag2a', 'wcag2aa'],
+          },
+        },
+        logViolations,
+      );
+})
+
