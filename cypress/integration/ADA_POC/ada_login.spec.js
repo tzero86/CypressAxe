2c823e44b3847292426cda97eab6e8e6752ad0b6
tzero86 <eliasmedina86@gmail.com>
2021-06-23 18:52:15 -0300

    Basic logged-in user test

---
+++ cypress/integration/ADA_POC/ada_login.spec.js
@@ -0,0 +1,13 @@
+describe('User Logged in - Customer Dashboard ADA Testing.', () => {
+    it('The user dashboard should not have accesibility issues.', () => {
+        // we navigate cypress to the login page (this URL loads the test login form)
+        cy.visit('/HBEWeb')
+        // we type in the username, we don't need a password
+        cy.get('#TestLogin_username').type('customer1')
+        // we click on the login button
+        cy.get('#TestLogin_doTestLogin').click()
+        // finally we run the ADA test command and we send it the URL of the dashboard for example for it to test
+        cy.testAccessibility('/HBEWeb/DisplayAccountHomeTab')
+        
+    });
+});
