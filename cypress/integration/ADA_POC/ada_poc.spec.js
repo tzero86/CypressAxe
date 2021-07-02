d4511690a704d57ab01b7cd3400d57ba5767b674
Paglia <mpaglia@deloitte.com>
2021-07-02 11:48:32 -0300

    Added some environment variables to run tests with different parameters

--- cypress/integration/ADA_POC/ada_poc.spec.js
+++ cypress/integration/ADA_POC/ada_poc.spec.js
@@ -1,7 +1,9 @@
 describe('Automated Accessibility Testing POC', ()=>{
 
     it('This page should not have any major accessibility issues.', ()=>{
-        cy.testAccessibility('');
+        
+        cy.testAccessibility(Cypress.env('specificUrl'));
+        
     })
 
     //TODO: cover other scenarios or pages (single element, exclude elements, other pages of the app)
